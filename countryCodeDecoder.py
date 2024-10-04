6644c8bae02597a8fb571dcba6133981b8f4347a
dantep77 <danteparadis77@gmail.com>
2024-10-04 16:12:06 -0400

    added comments and changd formatting

--- countryCodeDecoder.py
+++ countryCodeDecoder.py
@@ -1,13 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+'''
+Creates a table to decode country codes from wikipedia
+'''
+
+#TODO: add more comments
+
 url = 'https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2'
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'lxml')
 
 decoderDict = {}
-table = soup.find('table', {'class':'wikitable sortable sort-under'})
+table = soup.find('table', {'class':'wikitable sortable sort-under'}) #table containing rows of country codes and names
 rows = table.findAll('tr')
 
 for row in rows[1:]:
--- fakePersonGenerator.py
+++ fakePersonGenerator.py
@@ -1,41 +1,90 @@
 import requests
 import countryCodeDecoder # type: ignore
 
-randomUserUrl = 'https://randomuser.me/api/'
-genderUrl = 'https://api.genderize.io/?name='
-ethnicityUrl = 'https://api.nationalize.io/?name='
-
 def getRandomNames() -> dict:
-    response = requests.get(randomUserUrl)
+    '''
+    Uses the randomuser.me api to return a random person's name
+
+    Returns
+    -------
+    a dictionary containing the persons title, first name, and last name
+    '''
+    response = requests.get('https://randomuser.me/api/')
     randomUserData = response.json()
     return randomUserData['results'][0]["name"]
 
 def appendNameToUrl(url: str, name: str) -> str:
+    '''
+    appends the second parameter to the first, used to generate the correct query for the gender and nationality APIs
+
+    Parameters
+    ----------
+    url: str
+    name (first or last): str
+
+    Returns
+    --------
+    string containing the concatenated url
+    '''
     url += name
     return url
 
 def guessGender(firstName: str) -> str:
-    url = appendNameToUrl(genderUrl, firstName)
+    '''
+    Guesses the gender of the given first name using genderize.io API
+
+    Paramater
+    ---------
+    the first name of the person
+
+    Returns
+    --------
+    string contaning either 'male' or 'female'
+    '''
+    url = appendNameToUrl('https://api.genderize.io/?name=', firstName)
     response = requests.get(url)
     genderGuessData = response.json()
     return genderGuessData['gender']
 
 def guessEthnicity(lastName: str) -> str:
-    url = appendNameToUrl(ethnicityUrl, lastName)
+    '''
+    Guesses the ethnicity of a given last name using nationalize.io API
+
+    Parameter
+    ---------
+    last name of the person
+
+    Returns
+    -------
+    the country code of the country they are from
+    '''
+    url = appendNameToUrl('https://api.nationalize.io/?name=', lastName)
     response = requests.get(url)
     ethnicityGuessData = response.json()
     return ethnicityGuessData['country'][0]['country_id']
 
 def decodeCountryID(id: str) -> str:
+    '''
+    Decodes the given country code using the country code decoder
+
+    Parameter
+    ---------
+    2 character country code 
+    ex: US
+
+    Returns
+    -------
+    The full name of the country
+    '''
     decoder = countryCodeDecoder.decoderDict
     return decoder[id]
 
 def main():
-    randomNames = getRandomNames()
+    randomNames = getRandomNames() #gets table of random names
     firstName: str = randomNames['first']
     lastName: str = randomNames['last']
     gender: str = guessGender(firstName)
-    ethnicity: str = decodeCountryID(guessEthnicity(lastName))
+    ethnicity: str = decodeCountryID(guessEthnicity(lastName)) #decodes country code from last name
     print(f'{firstName} {lastName} is a {gender} from {ethnicity}')
 
 if __name__ == '__main__':
